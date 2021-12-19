208694c06e356c7e632fb83c1ddaae9fe4f44962
Anthony Jones <randomgeekdom@gmail.com>
2021-12-18 22:58:10 -0600

    adding material design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'homecomponent',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   title = 'Orchid Cavalry - Home';
