1536241d33a771945c3bbe0f66484b3bfac8b7d7
Anthony Jones <randomgeekdom@gmail.com>
2021-12-23 22:31:26 -0600

    style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title:string  = 'orchid-cavalry';
+  title: string = 'orchid-cavalry';
   game: Game | undefined;
 
   constructor(
-    private gameRepository: GameRepository, private assignmentGenerator: AssignmentGenerator,  private pubsubService: PubsubService, public router:Router){
+    private gameRepository: GameRepository, private assignmentGenerator: AssignmentGenerator, private pubsubService: PubsubService, public router: Router) {
   }
 
-   public get isLoaded(){
-     return !!this.game;
-   }
+  public get isLoaded() {
+    return !!this.game;
+  }
 
   ngOnInit() {
     this.game = this.gameRepository.GetGame();
   }
 
-  StartGame(){
+  StartGame() {
     var result = prompt("What is your character's first name?");
 
-    if(!result?.trim()){
+    if (!result?.trim()) {
       return;
     }
 
     this.game = new Game();
-    
+
     var unit = new Unit();
     unit.Name = `${result} Orchid`;
     unit.Description = "Orchid Cavalry Commander";
@@ -45,12 +45,12 @@ export class AppComponent {
     this.gameRepository.SaveGame(this.game);
   }
 
-  NextTurn(){
-    if(!!this.game){
+  NextTurn() {
+    if (!!this.game) {
       this.assignmentGenerator.GetNewAssignments(this.game);
       this.gameRepository.SaveGame(this.game);
-      
-      this.pubsubService.publish( 
+
+      this.pubsubService.publish(
         new NextTurnMessage()
       );
     }
